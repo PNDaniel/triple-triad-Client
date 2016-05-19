39bd07df9b2c5b2576e0fe3ec3211101b8f4267b
ivolimasilva <ivolimasilva@outlook.com>
2016-05-19 12:45:39 +0100

    Navbar css injected dynamically.

--- app/components/controllers/ctrlNavbar.js
+++ app/components/controllers/ctrlNavbar.js
@@ -3,15 +3,18 @@
     'use strict';
 
     // Created the controller to the navbar component
-    var CtrlNavbar = function () {
+    var CtrlNavbar = function (cssInjector) {
 
         // Message log to check if navbar component was loaded (delete for deploy)
         console.log('Navbar component controller loaded.');
 
+        // Inject CSS file dynamically
+        cssInjector.add('styles/navbar.css');
+
     };
 
     // Injecting modules used for better minifing later on
-    CtrlNavbar.$inject = [];
+    CtrlNavbar.$inject = ['cssInjector'];
 
     // Enabling the component in the app
     angular.module('triple-triad').component('navbar', {
